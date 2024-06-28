8891b66ac38961f6b2467a5b14715c4ce2f42182
ambikac <ambikac@localhost.localdomain>
2024-06-28 17:42:30 +0530

    added some code

--- runner.py
+++ runner.py
@@ -8,11 +8,23 @@ for program in programs:
     file = open(f"programs/{program}/manifest.json", "r")
     content = file.read()
     file.close()
-
+    print(content)
     program_object = json.loads(content)  
     entrypoint = program_object["entrypoint"]
+    print(entrypoint)
 
-    full_path = f"programs/{program}/{entrypoint}"
-    print(full_path)
+
+full_path = f"programs/{program}/{entrypoint}"
+print(full_path)
+
+tests = content["tests"]
+for test in tests:
+    description = test['description']
+    input_data = test['input']
+    expected_output = test['output']
+    print(f"Description: {description}")
+    print(f"Input: {input_data}")
+    print(f"Expected Output: {expected_output}")
+
     
 
